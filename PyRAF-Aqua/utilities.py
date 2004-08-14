d845e38a1b14938a97345ef52449d746b612ad6d
fpierfed <fpierfed@c60fa447-06dc-0310-afde-f9b22f87a6d9>
2004-08-14 23:02:29 +0000

    PyRAF needs some env. variables to be set (TERM, iraf, IRAFARCH etc.)

--- PyRAF-Aqua/utilities.py
+++ PyRAF-Aqua/utilities.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import traceback
 import sets
@@ -10,3 +11,15 @@ from objc import YES, NO, selector
 from Foundation import *
 from AppKit import *
 from PyObjCTools import NibClassBuilder
+
+# PyRAF needs the env. variables TERM, iraf, IRAFARCH
+if (not os.environ.has_key ('TERM')):
+    os.environ['TERM'] = 'vt100'
+if (not os.environ.has_key ('iraf')):
+    os.environ['iraf'] = '/iraf/iraf'
+if (not os.environ.has_key ('IRAFARCH')):
+    os.environ['IRAFARCH'] = 'macosx'
+
+# We need to access login.cl
+
+
